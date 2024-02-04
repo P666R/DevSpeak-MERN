c7cbe15d45814ef2c8e02d33ef71e6c9c4dd9c9e
PRoy <sam469roger@gmail.com>
2024-02-04 12:31:09 +0530

    feat-main: created & added functionality to signin page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,5 +1,125 @@
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
 function SignIn() {
-  return <div>Signin</div>;
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  //! controlled inputs
+  function handleChange(e) {
+    setFormData((curr) => ({ ...curr, [e.target.id]: e.target.value.trim() }));
+  }
+
+  //! submit form
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    //! validate form data
+    if (!formData.email || !formData.password)
+      return setErrorMessage('Please fill in all fields');
+
+    //! create user
+    try {
+      setIsLoading(true);
+      setErrorMessage(null);
+
+      const res = await fetch('/api/v1/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if (data.status !== 'success') {
+        return setErrorMessage(data.message);
+      }
+
+      setIsLoading(false);
+
+      if (res.ok && res.status === 200) {
+        setFormData({});
+        navigate('/');
+      }
+    } catch (err) {
+      setErrorMessage(err.message);
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* left */}
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+              DevSpeak
+            </span>
+            Blog
+          </Link>
+          <p className="text-sm mt-5">
+            This is the DevSpeak blog. Please sign in to write your own posts.
+            You can sign in with your email and password or with Google.
+          </p>
+        </div>
+        {/* right */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <div>
+              <Label htmlFor="email" value="Your email" />
+              <TextInput
+                type="email"
+                placeholder="Email"
+                id="email"
+                autoComplete="off"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label htmlFor="password" value="Your password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+              />
+            </div>
+            <Button
+              gradientDuoTone={'purpleToPink'}
+              type="submit"
+              disabled={isLoading}
+            >
+              {isLoading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                'Sign In'
+              )}
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Don&apos;t have an account?</span>
+            <Link to="/sign-up" className="text-blue-500">
+              Sign Up
+            </Link>
+          </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SignIn;
