e21bfb050050b706a5bd969a9a08b9ebf4915da8
PRoy <sam469roger@gmail.com>
2024-03-19 13:11:58 +0530

    completed about and projects page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,42 @@
 function About() {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            DevSpeak Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to DevSpeak, your go-to destination for all things related
+              to software development, technology trends, coding tips, and much
+              more. Whether you&apos;re a seasoned developer or just starting
+              your journey in the world of programming, DevSpeak is here to
+              inspire, educate, and empower you.
+            </p>
+
+            <p>
+              On this blog, discover a wealth of articles, tutorials, and
+              insights covering a wide range of topics, including web
+              development, mobile apps, artificial intelligence, machine
+              learning, cybersecurity, and cloud computing. Our expert
+              contributors share their knowledge and experience to help you stay
+              ahead in the rapidly evolving tech industry.
+            </p>
+
+            <p>
+              Ready to dive into the exciting world of software development?
+              Start exploring DevSpeak today and unlock a treasure trove of
+              resources to fuel your passion for technology. Whether you&apos;re
+              looking to sharpen your coding skills, stay updated with the
+              latest tech news, or connect with fellow developers, DevSpeak has
+              something for everyone.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
